fd2982fb63b86c4c635999f9cd16b669cae5c43e
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-05-31 12:55:06 +0530

    Add client uri

--- app.js
+++ app.js
@@ -347,7 +347,7 @@ const io = new SocketServer({
     cors: '*'
 });
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.CLIENT_URL,
     credentials: true
 }));
 io.attach(server);
